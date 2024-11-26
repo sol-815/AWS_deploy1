5f0ebc115072bd4a07521c5899776572e4809ea2
sol-815 <solsolsol815@gmail.com>
2024-11-26 12:37:46 +0900

    v 1.1

--- src/main/java/com/lec/spring/HomeController.java
+++ src/main/java/com/lec/spring/HomeController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/aws/v1")
+    @GetMapping("/aws/v1.1")
     public String hello(@RequestParam(defaultValue = "1") Integer num) {
         if(num == 1)
-            log.info("/aws/v1 호출. info 로그 #################");
+            log.info("/aws/v1.1 호출. info 로그 #################");
         else if(num == -1)
-            log.error("/aws/v1 호출. error 로그 #################");
+            log.error("/aws/v1.1 호출. error 로그 #################");
         else if(num == 0)
-            log.warn("/aws/v1 호출. warn 로그 #################");
-        return "<h1>AWS 배포 v1</h1>";
+            log.warn("/aws/v1.1 호출. warn 로그 #################");
+        return "<h1>AWS 배포 v1.1</h1>";
     }
 }
